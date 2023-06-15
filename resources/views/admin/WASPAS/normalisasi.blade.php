8d62653aa50e91554b9d3ca35bb08cfedef99301
RosisHudaya <rosishudayaputra02@gmail.com>
2023-06-15 20:29:15 +0700

    Add max, min, and ranking

--- resources/views/admin/WASPAS/normalisasi.blade.php
+++ resources/views/admin/WASPAS/normalisasi.blade.php
@@ -1,5 +1,68 @@
 <div class="card shadow mb-4">
     <div class="card-header py-3">
+        <h5 class="m-0 font-weight-bold text-primary">Max</h5>
+    </div>
+    <div class="card-body">
+        <table class="table table-bordered" id="" width="100%" cellspacing="0">
+            <thead>
+                <tr>
+                    <th>C1</th>
+                    <th>C2</th>
+                    <th>C3</th>
+                    <th>C4</th>
+                    <th>C5</th>
+                    <th>C6</th>
+                    <th>C7</th>
+                    <th>C8</th>
+                    <th>C9</th>
+                    <th>C10</th>
+                </tr>
+            </thead>
+            <tbody>
+            <tbody>
+                <td>{{ number_format($C1max['mapel'], 1) }}</td>
+                <td>{{ number_format($C2max['mapel'], 1) }}</td>
+                <td>{{ number_format($C3max['mapel'], 1) }}</td>
+                <td>{{ number_format($C4max['mapel'], 1) }}</td>
+                <td>{{ number_format($C5max['mapel'], 1) }}</td>
+                <td>{{ number_format($C6max['mapel'], 1) }}</td>
+                <td>{{ number_format($C7max['mapel'], 1) }}</td>
+                <td>{{ number_format($C8max['mapel'], 1) }}</td>
+                <td>{{ number_format($C9max['mapel'], 1) }}</td>
+                <td>{{ number_format($C10max['mapel'], 1) }}</td>
+
+                </tr>
+            </tbody>
+
+            </tbody>
+        </table>
+    </div>
+</div>
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h5 class="m-0 font-weight-bold text-primary">Min</h5>
+    </div>
+    <div class="card-body">
+        <table class="table table-bordered" id="" width="100%" cellspacing="0">
+            <thead>
+                <tr>
+                    <th>C11</th>
+                    <th>C12</th>
+                </tr>
+            </thead>
+            <tbody>
+            <tbody>
+                <td>{{ number_format($C11min['mapel'], 1) }}</td>
+                <td>{{ number_format($C12min['mapel'], 1) }}</td>
+                </tr>
+            </tbody>
+
+            </tbody>
+        </table>
+    </div>
+</div>
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
         <h5 class="m-0 font-weight-bold text-primary">Normalisasi</h5>
     </div>
     <div class="card-body">
@@ -28,18 +91,18 @@
 
 
                 <td>{{ $item->nama }}</td>
-                <td>{{ $item->C1 / $C1max['mapel'] }}</td>
-                <td>{{ $item->C2 / $C2max['mapel'] }}</td>
-                <td>{{ $item->C3 / $C3max['mapel'] }}</td>
-                <td>{{ $item->C4 / $C4max['mapel'] }}</td>
-                <td>{{ $item->C5 / $C5max['mapel'] }}</td>
-                <td>{{ $item->C6 / $C6max['mapel'] }}</td>
-                <td>{{ $item->C7 / $C7max['mapel'] }}</td>
-                <td>{{ $item->C8 / $C8max['mapel'] }}</td>
-                <td>{{ $item->C9 / $C9max['mapel'] }}</td>
-                <td>{{ $item->C10 / $C10max['mapel'] }}</td>
-                <td>{{ $C11min['mapel'] /  $item->C11}}</td>
-                <td>{{ $C12min['mapel'] /  $item->C12}}</td>
+                <td>{{ number_format($item->C1 / $C1max['mapel'], 3) }}</td>
+                <td>{{ number_format($item->C2 / $C2max['mapel'], 3) }}</td>
+                <td>{{ number_format($item->C3 / $C3max['mapel'], 3) }}</td>
+                <td>{{ number_format($item->C4 / $C4max['mapel'], 3) }}</td>
+                <td>{{ number_format($item->C5 / $C5max['mapel'], 3) }}</td>
+                <td>{{ number_format($item->C6 / $C6max['mapel'], 3) }}</td>
+                <td>{{ number_format($item->C7 / $C7max['mapel'], 3) }}</td>
+                <td>{{ number_format($item->C8 / $C8max['mapel'], 3) }}</td>
+                <td>{{ number_format($item->C9 / $C9max['mapel'], 3) }}</td>
+                <td>{{ number_format($item->C10 / $C10max['mapel'], 3) }}</td>
+                <td>{{ number_format($C11min['mapel'] / $item->C11, 3) }}</td>
+                <td>{{ number_format($C12min['mapel'] / $item->C12, 3) }}</td>
 
                 </tr>
             </tbody>
